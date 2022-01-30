6e8f179c90d69bcf6991137c4408f07472cda5f5
kbugz <kbugusky@gmail.com>
2022-01-30 10:52:33 -0600

    current time display feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
-
 //Moment.js code for current date and time
 let dateEl = document.getElementById("currentDay");
 let NoMo = moment().format('MMMM Do YYYY, h:mm:ss a');
 dateEl.innerHTML = NoMo;
-var curHour = moment().format("HH");
-// window.localStorage.setItem("date", curHour);
-// console.log(curHour);
-// console.log(moment());
+var curHour = moment().format("HH"); // get the current hour through moment
+curHour = parseInt(curHour, 10); // Change hour string into hour number
 
-// Compare each time with the current. 
+
+// Compare each time with the current and update the box.  
 $(".time-div").each(function () { // for each .time-div
     console.log($(this).attr("id"));
     var timeDiv = $(this).attr("id");
@@ -17,9 +15,12 @@ $(".time-div").each(function () { // for each .time-div
         $(this).children(".description").addClass("white-text");
     } else if (curHour < timeDiv) {
         $(this).removeClass("present");
+        $(this).removeClass("past");
         $(this).addClass("future");
     } else if (curHour > timeDiv) {
         $(this).removeClass("future");
+        $(this).removeClass("present");
         $(this).addClass("past");
     }
 });
+//
